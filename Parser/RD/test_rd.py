d244e6638c0ebfa14be62d5cd765b9d8c6cf8328
ahmeddsakrr <ahmed.sakr9052@gmail.com>
2023-05-23 20:59:36 +0300

    Handled Errors in RD and Added

--- Parser/RD/test_rd.py
+++ Parser/RD/test_rd.py
@@ -1,6 +1,7 @@
 import unittest
 
 import nltk
+import pandas
 from nltk.draw import TreeWidget
 from nltk.draw.util import CanvasFrame
 
@@ -8,6 +9,8 @@ from Parser.RD.rd_parser import RDParser
 from Scanner.scanner import Scanner
 
 import traceback
+import tkinter as tk
+import pandastable as pt
 
 
 def draw_nltk_tree(tree):
@@ -18,6 +21,12 @@ def draw_nltk_tree(tree):
     cf.destroy()
     nltk.draw.tree.draw_trees(tree)
 
+def display_error_list(error_list):
+    df1 = pandas.DataFrame(error_list)
+    dTDa2 = tk.Toplevel()
+    dTDa2.title('Error List')
+    dTDaPT2 = pt.Table(dTDa2, dataframe=df1, showtoolbar=True, showstatusbar=True)
+    dTDaPT2.show()
 
 class ParserTest(unittest.TestCase):
     def test_one_statement(self):
@@ -27,6 +36,7 @@ class ParserTest(unittest.TestCase):
         parser = RDParser(tokens)
         try:
             result = parser.parse()
+            display_error_list(parser.errors)
             draw_nltk_tree(result)
             result.pretty_print()
         except Exception as e:
@@ -233,6 +243,96 @@ class ParserTest(unittest.TestCase):
             result.pretty_print()
         except Exception as e:
             print(f"Exception occurred during parsing: {e}")
+
+##############################################################################################################
+    #Invalid test cases
+    def test_missing_semicolon(self):
+        source_code = "program test begin x := 10; end."
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+    def test_missing_period(self):
+        source_code = "program test; begin x := 10; end"
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+    def test_missing_begin(self):
+        source_code = "program test; x := 10; end."
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+    def test_missing_end(self):
+        source_code = "program test; begin x := 10; ."
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+    def test_missing_program(self):
+        source_code = "test; begin x := 10; end."
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+    def test_missing_identifier(self):
+        source_code = "program; begin x := 10; end."
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+    def test_misspelled_program(self):
+        source_code = "programm test; begin x := 10; end."
+        scanner = Scanner()
+        tokens = scanner.scan(source_code)
+        parser = RDParser(tokens)
+        try:
+            result = parser.parse()
+            draw_nltk_tree(result)
+            result.pretty_print()
+        except Exception as e:
+            print(f"Exception occurred during parsing: {e}")
+
+
+
+
 
 
 if __name__ == '__main__':
